cc1f5016b8371d6dd403c56e56f0c74082d92876
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-03-31 21:58:45 +0200

    Create base look

--- src/components/SalaryCalculator/SalaryCalculator.jsx
+++ src/components/SalaryCalculator/SalaryCalculator.jsx
@@ -1,5 +1,25 @@
+import Buttons from "./components/Buttons";
+import Input from "./components/TextInput";
+import Allowances from "./components/Allowances";
+import RngeSlider from "./components/RngeSlider";
+
 const SalaryCalculator = () => {
-  return <div className="bg-red-300">SalaryCalculator</div>;
+  const brutto = 100;
+
+  const netto = brutto;
+  return (
+    <div className="bg-red-300">
+      <p>SalaryCalculator</p>
+      <Input title="cs" label="neve"></Input>
+      <Input title="bér" label="bére"></Input>
+      <RngeSlider></RngeSlider>
+      <Buttons></Buttons>
+
+      <Allowances></Allowances>
+      <p>Számított nettó bér:</p>
+      <p>{netto} Ft</p>
+    </div>
+  );
 };
 
 export default SalaryCalculator;
