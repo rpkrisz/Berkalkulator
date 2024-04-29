5cd18193670e93c0d4549c5cf91cfd3afb594f32
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-29 13:18:00 +0200

    Add base table

--- src/components/HouseholdSummary/HouseholdSummary.jsx
+++ src/components/HouseholdSummary/HouseholdSummary.jsx
@@ -1,5 +1,27 @@
-const HouseholdSummary = () => {
-  return <div className="bg-yellow-300">HouseholdSummary</div>;
+const HouseholdSummary = ({ netto, name }) => {
+  return (
+    <div className="bg-yellow-300">
+      <h1>HouseholdSummary</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Családtag</th>
+            <th>Nettó bér</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{name}</td>
+            <td>{netto} Ft</td>
+          </tr>
+          <tr>
+            <td>Összesen</td>
+            <td>{netto} Ft</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default HouseholdSummary;
