8f3ca32390903f44a00511d99e05a882f4d66adb
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-29 13:26:13 +0200

    Add min/max to range and setter

--- src/components/SalaryCalculator/components/RngeSlider.jsx
+++ src/components/SalaryCalculator/components/RngeSlider.jsx
@@ -1,13 +1,20 @@
-function RngeSlider({ title, label }) {
+function RngeSlider({ brutto, setter }) {
   function handelChange(e) {
-    console.log(e.target.value);
+    setter(Number(e.target.value));
   }
   return (
     <div>
-      <label htmlFor="default-range" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-        Default range
-      </label>
-      <input onChange={handelChange} id="default-range" name="default-range" type="range" className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700" />
+      <label htmlFor="salaryrange" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
+      <input
+        onChange={handelChange}
+        id="salaryrange"
+        name="salaryrange"
+        value={brutto}
+        type="range"
+        className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+        min={0}
+        max={3000000}
+      />
     </div>
   );
 }
