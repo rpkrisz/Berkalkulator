a08e6a5cd61f2c1c62fff4d8ac1eb8cff4604f25
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-29 13:43:14 +0200

    Add allowances array

--- src/components/SalaryCalculator/SalaryCalculator.jsx
+++ src/components/SalaryCalculator/SalaryCalculator.jsx
@@ -2,9 +2,10 @@ import Buttons from "./components/Buttons";
 import TextInput from "./components/TextInput";
 import Allowances from "./components/Allowances";
 import RngeSlider from "./components/RngeSlider";
+import { useState } from "react";
 
 function calculateNetto(brutto) {
-  let netto = 0;
+  let netto = brutto;
   netto = brutto - brutto * 0.15 - brutto * 0.185;
   return netto;
 }
@@ -16,13 +17,15 @@ function calculateNetto(brutto) {
  */
 const SalaryCalculator = ({ brutto, name, netto, setterB, setterN, setterNet }) => {
   setterNet(calculateNetto(Number(brutto)));
+  const [allowances, setAllowances] = useState([false, false, false, false]);
+  
   return (
     <div className="bg-red-300">
       <TextInput title="Családtag neve" label="családtag nevét" value={name} setter={setterN}></TextInput>
       <TextInput title="Bruttó bér" label="bruttó bérét" value={brutto} setter={setterB}></TextInput>
       <RngeSlider brutto={brutto} setter={setterB}></RngeSlider>
       <Buttons brutto={brutto} setter={setterB}></Buttons>
-      <Allowances netto={netto} setter={setterNet}></Allowances>
+      <Allowances allowances={allowances} setter={setAllowances}></Allowances>
       <p>Számított nettó bér:</p>
       <p>{netto ?? 0} Ft</p>
     </div>
--- src/components/SalaryCalculator/components/Allowances.jsx
+++ src/components/SalaryCalculator/components/Allowances.jsx
@@ -1,12 +1,12 @@
 import AllowanceToggel from "./AllowanceToggel.jsx";
-function Allowances() {
+function Allowances({ allowances, setter }) {
   return (
     <div className="flex flex-col justify-start">
       <h1>KEDVEZMÉNYEK</h1>
-      <AllowanceToggel label="25 év"></AllowanceToggel>
-      <AllowanceToggel label="Friss"></AllowanceToggel>
-      <AllowanceToggel label="Személyi"></AllowanceToggel>
-      <AllowanceToggel label="Családi"></AllowanceToggel>
+      <AllowanceToggel allowances={allowances} setter={setter} index={0} label="25 év"></AllowanceToggel>
+      <AllowanceToggel allowances={allowances} setter={setter} index={1} label="Friss"></AllowanceToggel>
+      <AllowanceToggel allowances={allowances} setter={setter} index={2} label="Személyi"></AllowanceToggel>
+      <AllowanceToggel allowances={allowances} setter={setter} index={3} label="Családi"></AllowanceToggel>
     </div>
   );
 }
