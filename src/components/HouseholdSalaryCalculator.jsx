53d6ab13ab320486f8825dfcae21b0ba198a754a
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-29 13:16:56 +0200

    Add app states

--- src/components/HouseholdSalaryCalculator.jsx
+++ src/components/HouseholdSalaryCalculator.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import FamilyMemberTabs from "./FamilyMemberTabs/FamilyMemberTabs";
 import HouseholdSummary from "./HouseholdSummary/HouseholdSummary";
 import SalaryCalculator from "./SalaryCalculator/SalaryCalculator";
 
 const HouseholdSalaryCalculator = () => {
+  const [brutto, setBrutto] = useState(0);
+  const [netto, setNetto] = useState(0);
+  const [name, setName] = useState("");
+
   return (
     <>
       <header>
         <FamilyMemberTabs />
       </header>
       <main className="flex flex-row justify-start">
-        <SalaryCalculator />
-        <HouseholdSummary />
+        <SalaryCalculator brutto={brutto} name={name} netto={netto} setterB={setBrutto} setterN={setName} setterNet={setNetto} />
+        <HouseholdSummary netto={netto} name={name} />
       </main>
     </>
   );
