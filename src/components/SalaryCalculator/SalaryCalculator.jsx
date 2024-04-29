13d5d2926387a4f9efe83f8e68f1e59b80085383
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-29 13:29:44 +0200

    Calculate Netto and add child parms

--- src/components/SalaryCalculator/SalaryCalculator.jsx
+++ src/components/SalaryCalculator/SalaryCalculator.jsx
@@ -1,23 +1,30 @@
 import Buttons from "./components/Buttons";
-import Input from "./components/TextInput";
+import TextInput from "./components/TextInput";
 import Allowances from "./components/Allowances";
 import RngeSlider from "./components/RngeSlider";
 
-const SalaryCalculator = () => {
-  const brutto = 100;
+function calculateNetto(brutto) {
+  let netto = 0;
+  netto = brutto - brutto * 0.15 - brutto * 0.185;
+  return netto;
+}
 
-  const netto = brutto;
+/**
+ *
+ * @param {{brutto:number, name:string,netto:number, setterB:Function, setterN:Function, setterNet:Function}} param0
+ * @returns
+ */
+const SalaryCalculator = ({ brutto, name, netto, setterB, setterN, setterNet }) => {
+  setterNet(calculateNetto(Number(brutto)));
   return (
     <div className="bg-red-300">
-      <p>SalaryCalculator</p>
-      <Input title="cs" label="neve"></Input>
-      <Input title="bér" label="bére"></Input>
-      <RngeSlider></RngeSlider>
-      <Buttons></Buttons>
-
-      <Allowances></Allowances>
+      <TextInput title="Családtag neve" label="családtag nevét" value={name} setter={setterN}></TextInput>
+      <TextInput title="Bruttó bér" label="bruttó bérét" value={brutto} setter={setterB}></TextInput>
+      <RngeSlider brutto={brutto} setter={setterB}></RngeSlider>
+      <Buttons brutto={brutto} setter={setterB}></Buttons>
+      <Allowances netto={netto} setter={setterNet}></Allowances>
       <p>Számított nettó bér:</p>
-      <p>{netto} Ft</p>
+      <p>{netto ?? 0} Ft</p>
     </div>
   );
 };
