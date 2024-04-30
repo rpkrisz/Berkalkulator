826dab4dbf03a4407a64b2e5499829e2d609d8dd
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-30 08:55:12 +0200

    Delete title

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import HouseholdSalaryCalculator from "./components/HouseholdSalaryCalculator";
 function App() {
   return (
     <div>
-      <h1>Bérkalkulátor alkalmazás</h1>
       <HouseholdSalaryCalculator />
     </div>
   );
