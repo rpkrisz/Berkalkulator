fec9303844eb174dc0c07cc0fbac2c1a9f343500
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-30 20:57:52 +0200

    Add member tabs

--- src/components/FamilyMemberTabs/FamilyMemberTabs.jsx
+++ src/components/FamilyMemberTabs/FamilyMemberTabs.jsx
@@ -1,5 +1,84 @@
-const FamilyMemberTabs = () => {
-  return <div className="bg-blue-300">FamilyMemberTabs</div>;
-};
+import { useState, useEffect } from "react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import SalaryCalculator from "../SalaryCalculator/SalaryCalculator";
+
+function calculateNetto(brutto, allowances, peoples) {
+  brutto = Number(brutto);
+  let newBrutto = brutto;
+  let szja = brutto * 0.15;
+  let tb = brutto * 0.185;
+  let tax = szja + tb;
+  let bonus = 0;
+
+  if (allowances[0]) {
+    brutto - 499952 > 0 ? (newBrutto = brutto - 499952) : (newBrutto = 0);
+    szja = newBrutto * 0.15;
+    tax = szja + tb;
+  }
+  if (allowances[1] && allowances[4]) {
+    bonus = 5000;
+  }
+  if (allowances[2]) {
+    tax - 77300 > 0 ? (tax = tax - 77300) : (tax = 0);
+  }
+  if (allowances[3]) {
+    let allowance = 0;
+    switch (peoples["beneficiary"]) {
+      case 1:
+        allowance = 10000 * peoples["dependant"];
+        break;
+      case 2:
+        allowance = 20000 * peoples["dependant"];
+        break;
+      case 3:
+        allowance = 33000 * peoples["dependant"];
+        break;
+
+      default:
+        break;
+    }
+
+    tax - allowance > 0 ? (tax = tax - allowance) : (tax = 0);
+  }
+
+  let netto = brutto - tax + bonus;
+  return netto;
+}
+
+function FamilyMemberTabs({ members, setter }) {
+  const [member, setMember] = useState({ name: "", brutto: 0, netto: 0 });
+  const { name, brutto, netto } = member;
+  const [allowances, setAllowances] = useState([false, false, false, false, false]);
+  const [peoples, setPeoples] = useState({ dependant: 0, beneficiary: 0 });
+
+  // update netto when change happens
+  useEffect(() => {
+    const newNetto = calculateNetto(Number(brutto), allowances, peoples);
+    const newMember = { ...member, netto: newNetto };
+    setMember(newMember);
+  }, [brutto, allowances, peoples]);
+
+  useEffect(() => {
+    members[0] = member;
+    setter([...members]);
+  }, [member]);
+
+  return (
+    <Tabs>
+      <TabList>
+        {members.map((tab, index) => (
+          <Tab key={index}>{tab.name}</Tab>
+        ))}
+      </TabList>
+      <TabPanels>
+        {members.map((tab, index) => (
+          <TabPanel key={index}>
+            <SalaryCalculator member={member} setter={setMember} allowances={allowances} setAllowances={setAllowances} peoples={peoples} setPeoples={setPeoples} />
+          </TabPanel>
+        ))}
+      </TabPanels>
+    </Tabs>
+  );
+}
 
 export default FamilyMemberTabs;
--- src/components/HouseholdSalaryCalculator.jsx
+++ src/components/HouseholdSalaryCalculator.jsx
@@ -1,76 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import FamilyMemberTabs from "./FamilyMemberTabs/FamilyMemberTabs";
 import HouseholdSummary from "./HouseholdSummary/HouseholdSummary";
-import SalaryCalculator from "./SalaryCalculator/SalaryCalculator";
-
-function calculateNetto(brutto, allowances, peoples) {
-  brutto = Number(brutto);
-  let newBrutto = brutto;
-  let szja = brutto * 0.15;
-  let tb = brutto * 0.185;
-  let tax = szja + tb;
-  let bonus = 0;
-
-  if (allowances[0]) {
-    brutto - 499952 > 0 ? (newBrutto = brutto - 499952) : (newBrutto = 0);
-    szja = newBrutto * 0.15;
-    tax = szja + tb;
-  }
-  if (allowances[1] && allowances[4]) {
-    bonus = 5000;
-  }
-  if (allowances[2]) {
-    tax - 77300 > 0 ? (tax = tax - 77300) : (tax = 0);
-  }
-  if (allowances[3]) {
-    let allowance = 0;
-    switch (peoples["beneficiary"]) {
-      case 1:
-        allowance = 10000 * peoples["dependant"];
-        break;
-      case 2:
-        allowance = 20000 * peoples["dependant"];
-        break;
-      case 3:
-        allowance = 33000 * peoples["dependant"];
-        break;
-
-      default:
-        break;
-    }
-
-    tax - allowance > 0 ? (tax = tax - allowance) : (tax = 0);
-  }
-
-  let netto = brutto - tax + bonus;
-  return netto;
-}
 
 const HouseholdSalaryCalculator = () => {
-  const [member, setMember] = useState({ name: "", brutto: 0, netto: 0 });
-  const { name, brutto, netto } = member;
   const [members, setMembers] = useState([{ name: "", brutto: 0, netto: 0 }]);
-  const [allowances, setAllowances] = useState([false, false, false, false, false]);
-  const [peoples, setPeoples] = useState({ dependant: 0, beneficiary: 0 });
 
-  // update netto when change happens
-  useEffect(() => {
-    const newNetto = calculateNetto(Number(brutto), allowances, peoples);
-    const newMember = { ...member, netto: newNetto };
-    setMember(newMember);
-  }, [brutto, allowances, peoples]);
-  useEffect(() => {
-    members[0] = member;
-    setMembers([...members]);
-  }, [member]);
   return (
     <>
-      {console.log(members)}
-      <header>
-        <FamilyMemberTabs />
-      </header>
       <main className="flex flex-row justify-center w-full">
-        <SalaryCalculator member={member} setter={setMember} allowances={allowances} setAllowances={setAllowances} peoples={peoples} setPeoples={setPeoples} />
+        <FamilyMemberTabs members={members} setter={setMembers} />
         <HouseholdSummary members={members} />
       </main>
     </>
