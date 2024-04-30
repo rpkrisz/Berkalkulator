37a1320970741d7677ba7ccf43e922f97f331555
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-30 09:38:19 +0200

    Add modal and entitled components to newWedds allowance

--- src/components/SalaryCalculator/components/Allowances.jsx
+++ src/components/SalaryCalculator/components/Allowances.jsx
@@ -1,10 +1,31 @@
 import AllowanceToggel from "./AllowanceToggel.jsx";
+import Entitled from "./Entitled.jsx";
+import ModalDateInput from "./ModalDateInput.jsx";
+import { useDisclosure } from "@chakra-ui/react";
+import { useState } from "react";
+
 function Allowances({ allowances, setter }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <div className="flex flex-col justify-start">
+    <div className="flex flex-col justify-start gap-1">
       <h1>KEDVEZMÉNYEK</h1>
       <AllowanceToggel allowances={allowances} setter={setter} index={0} label="25 év"></AllowanceToggel>
-      <AllowanceToggel allowances={allowances} setter={setter} index={1} label="Friss"></AllowanceToggel>
+      <AllowanceToggel allowances={allowances} setter={setter} index={1} label="Friss">
+        <button onClick={onOpen} href="#" className="ui right floated green button rounded-full bg-blue-700 p-1" id="newModal">
+          <i className="plus icon"></i>
+          Dátum
+        </button>
+        <ModalDateInput
+          open={isOpen}
+          handelClose={() => {
+            onClose();
+          }}
+          allowances={allowances}
+          setter={setter}
+        />
+        <Entitled allowances={allowances} />
+      </AllowanceToggel>
       <AllowanceToggel allowances={allowances} setter={setter} index={2} label="Személyi"></AllowanceToggel>
       <AllowanceToggel allowances={allowances} setter={setter} index={3} label="Családi"></AllowanceToggel>
     </div>
