4e87834bdf54a5d21cf2b6858c5db37f0eda72b3
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-30 11:01:09 +0200

    Add button click handlers

--- src/components/SalaryCalculator/components/CounterInput.jsx
+++ src/components/SalaryCalculator/components/CounterInput.jsx
@@ -1,12 +1,29 @@
 import { Button, Input } from "@chakra-ui/react";
 
-export default function CounterInput() {
-    
+export default function CounterInput({ peoples, setPeoples, type }) {
+  const limit = 3;
+  function handelLeftClick() {
+    if (peoples[type] === 0) return;
+    const count = peoples[type] - 1;
+
+    if (peoples["beneficiary"] === peoples["dependant"] && type === "dependant") {
+      setPeoples({ dependant: count, beneficiary: count });
+      return;
+    }
+    setPeoples({ ...peoples, [type]: count });
+  }
+
+  function handelRightClick() {
+    if (peoples[type] >= limit) return;
+    if (peoples["beneficiary"] === peoples["dependant"] && type === "beneficiary") return;
+
+    setPeoples({ ...peoples, [type]: peoples[type] + 1 });
+  }
   return (
     <div className="flex align-middle">
-      <Button>-</Button>
-      <Input type="number" defaultValue={3} />
-      <Button>+</Button>
+      <Button onClick={handelLeftClick}>-</Button>
+      <Input type="number" value={peoples[type]} disabled />
+      <Button onClick={handelRightClick}>+</Button>
     </div>
   );
 }
