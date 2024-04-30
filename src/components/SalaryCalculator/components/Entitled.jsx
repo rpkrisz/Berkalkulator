568c2b4a4cae5e7249a9f1ac22da7188110c8b5d
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-30 20:33:08 +0200

    Small style changes

--- src/components/SalaryCalculator/components/Entitled.jsx
+++ src/components/SalaryCalculator/components/Entitled.jsx
@@ -1,10 +1,18 @@
 /**
- * 
- * @param {{allowances:Array}} 
- * @returns 
+ *
+ * @param {{allowances:Array}}
+ * @returns
  */
 function Entitled({ allowances }) {
-  return <>{allowances[allowances.length - 1] ? <div className="rounded-full bg-green-500 p-1">Jogosult</div> : <div className="rounded-full bg-red-600 p-1">Jogosulatlan</div>}</>;
+  return (
+    <>
+      {allowances[allowances.length - 1] ? (
+        <div className="rounded-full bg-green-500 p-1 pl-3 pr-3 flex align-text-bottom">Jogosult</div>
+      ) : (
+        <div className="rounded-full bg-red-600 p-1 pl-3 pr-3 flex align-text-bottom">Jogosulatlan</div>
+      )}
+    </>
+  );
 }
 
 export default Entitled;
