d06ba054d677b288bdddccde83f3a95eb438376b
Réthey-Prikkel <rp.krisz123@gmail.com>
2024-04-30 09:51:38 +0200

    Finish shell

--- src/components/SalaryCalculator/components/Dependants.jsx
+++ src/components/SalaryCalculator/components/Dependants.jsx
@@ -1,6 +1,14 @@
 export default function Dependants() {
-    return <div>
-        <button>+</button>
-        <input type="number" />
-  </div>;
+
+  return (
+    <div>
+      <button>-</button>
+      <input type="number" defaultValue={3} />
+      <button>+</button>
+      <p>Eltartott, ebből kedvezményezett:</p>
+      <button>-</button>
+      <input type="number" defaultValue={1} />
+      <button>+</button>
+    </div>
+  );
 }
